1c5e221b36fd177463074477b914b6e65417bbe3
okjuju <60528035+okjuju@users.noreply.github.com>
2022-05-05 11:41:18 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "784454559144796160"; // your league ID
-export const leagueName = "The Pigskin Players"; // your league name
+export const leagueName = "Pigskin Players"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
